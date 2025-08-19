ac7694700225fa9462ce3eae2e624149537b3ce5
Nando <fernanrods1002@gmail.com>
2025-08-19 13:44:04 -0300

    first commit

---
+++ src/main/java/com/fernando/wksmongo/WksmongoApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.wksmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WksmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WksmongoApplication.class, args);
+	}
+
+}
