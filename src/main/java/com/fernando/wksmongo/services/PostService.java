5f3d9c24c4b9786a7739580c78e564b4dec0f4a6
Nando <fernanrods1002@gmail.com>
2025-08-23 12:36:28 -0300

    post por id

---
+++ src/main/java/com/fernando/wksmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.fernando.wksmongo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernando.wksmongo.domain.Post;
+import com.fernando.wksmongo.repositories.PostRepository;
+import com.fernando.wksmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        //se não houver usuario, retorna nulo
+        Post post = repository.findById(id).orElse(null);
+        if (post == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return post;
+    }
+
+}
